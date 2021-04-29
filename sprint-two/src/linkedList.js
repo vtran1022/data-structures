18d788ac101742d43d467d38900d0203a013aa6e
Yichun Jin <jinyichun17@gmail.com>
2021-04-29 11:29:35 -0700

    working on likedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,48 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === undefined) {
+      list.head = Node(value);
+      list.tail = Node(value);
+    } else {
+      list.head = list.tail;
+      list.tail = Node(value);
+    }
+
   };
 
   list.removeHead = function() {
+    if (!list.head) {
+      return;
+    }
+    if (list.head === undefined) {
+      return;
+    }
+    // assign the tail value to a variable
+    var firstHead = list.head.value;
+    var container = [];
+    container.push(firstHead);
+    // delete head value
+    delete list.head;
+    if (list.tail) {
+      list.head = list.tail;
+    }
+    // return the variable
+    // debugger;
+
+    return container[0];
   };
 
   list.contains = function(target) {
+    // to check if both properties has the target value
+    if (list.head === target || list.tail === target) {
+    // if either has the target value
+      // return true
+      return true;
+    } else {
+      return false;
+    }
+    // return false
   };
 
   return list;
