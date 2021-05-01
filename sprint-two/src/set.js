c35351691c0b98f70fbfe4bbb54d7ad3c1ae685a
Yichun Jin <jinyichun17@gmail.com>
2021-04-30 18:56:50 -0700

    set complete!

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      this.storage.splice(i, 1);
+    }
+  }
 };
 
 /*
